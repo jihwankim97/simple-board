fb8a13c8cffc13846117b51bd201f45575a9af43
김지환 <gimjihwan@gimjihwan-ui-MacBookPro.local>
2024-09-12 23:29:21 +0900

    feat: 리스트 페이지 작성
    검색기능, 게시글 리스트, 예시 작성

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const handlebars = require("express-handlebars");
+const path = require("path")
 const app = express();
 const port=3000;
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.engine("handlebars", handlebars.engine());
 app.set("view engine", "handlebars");
@@ -9,7 +12,7 @@ app.set("views", __dirname + "/views");
 
 //라우터 설정
 app.get("/", (req,res)=>{
-    res.render("home", {title:"홈", message:"mmmmmeeeeesssssaaaaggggeeee"});
+    res.render("home", {title:"계시판1", message:"mmmmmeeeeesssssaaaaggggeeee"});
 })
 
 app.listen( port,()=>{
