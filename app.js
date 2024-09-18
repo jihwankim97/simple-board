b8c1723d68fd6034400614fecc8873633e8cd7cc
jihwankim97 <cool102476@naver.com>
2024-09-18 19:31:47 +0900

    feat: 댓글 추가&삭제기능 추가

--- app.js
+++ app.js
@@ -132,6 +132,54 @@ app.delete("/delete", async (req, res)=>{
     }
 })
 
+app.post("/write-comment", async (req, res)=>{
+    const {id, name, password, comment}=req.body;
+
+    const post = await postService.getPostById(collection, id);
+
+    if(post.comments){
+        post.comments.push({
+            idx: post.comments.length +1,
+            name,
+            password,
+            comment,
+            createDt: new Date().toISOString(),
+        });
+    }else{
+        post.comments = [
+            {
+                idx:1,
+                name,
+                password,
+                comment,
+                createDt: new Date().toISOString(),
+            },
+        ];
+    }
+    postService.updatePost(collection, id, post);
+    return res.redirect(`/detail/${id}`);
+})
+
+
+app.delete("/delete-comment", async (req, res) => {
+    const { id, idx, password } = req.body;
+  
+    const post = await collection.findOne(
+      {
+        _id: new ObjectId(id),
+        comments: { $elemMatch: { idx: parseInt(idx), password } },
+      },
+      postService.projectionOption
+    );
+  
+    if (!post) {
+      return res.json({ isSuccess: false });
+    }
+  
+    post.comments = post.comments.filter((comment) => comment.idx != idx);
+    postService.updatePost(collection, id, post);
+    return res.json({ isSuccess: true });
+  });
 
 
 
--- configs/handlebars-helpers.js
+++ configs/handlebars-helpers.js
@@ -1,5 +1,5 @@
 module.exports={
-    lengthOfList:(List=[])=>list.length,
+    lengthOfList:(List=[])=>List.length,
     eq:(val1, val2)=>val1===val2,
     dateString:(isoString)=>new Date(isoString).toLocaleDateString(),
 }
